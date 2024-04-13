a80aa2df1b1165e04d9cb2f90c69f9be3475c2bc
diyorkhud <d1yorkhud@gmail.com>
2024-04-13 12:17:13 +0500

    initial screen settings

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Task'),
+      ),
+      // body: // Implement the form to add a task here
+    );
+  }
+}
